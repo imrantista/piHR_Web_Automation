28b2129a3cb4b07b61ccc30f726752c0bc70e624
gmimran <mostafa.imran@vivasoftltd.com>
2025-11-25 11:56:54 +0600

    used login page locator in invalid login test

--- pages/Auth/InvalidLogin.js
+++ pages/Auth/InvalidLogin.js
@@ -1,21 +1,18 @@
 import BasePage from '../BasePage';
-import { config } from '../../config/testConfig.js';
+import { LoginPage } from './LoginPage.js';
 
 export class InvalidLogin extends BasePage {
   constructor(page, context) {
     super(page, context);
     this.page = page;
     this.context = context;
-    this.loginFrame = page.frameLocator('iframe[title="Login Page"]');
-    this.emailTxt = this.loginFrame.getByRole('textbox', { name: 'Username/ Mobile' });
-    this.passwordTxt = this.loginFrame.getByRole('textbox', { name: 'Password' });
-    this.loginBtn = this.loginFrame.getByRole('button', { name: 'Login' });
-    this.errorMsg = this.loginFrame.getByText('Invalid user name or password');
+    this.loginPage = new LoginPage(page, context);
+    this.errorMsg = page.locator('iframe[title="Login Page"]').contentFrame().getByText('Invalid user name or password');
   }
   async invalidLogin(username) {
-    await this.waitAndFill(this.emailTxt, username, 'Email');
-    await this.waitAndFill(this.passwordTxt, 'Password');
-    await this.expectAndClick(this.loginBtn, 'Login Button');
+    await this.waitAndFill(this.loginPage.emailTxt, username, 'Email');
+    await this.waitAndFill(this.loginPage.passwordTxt, 'invalidpass');
+    await this.expectAndClick(this.loginPage.loginBtn, 'Login Button');
     await this.assert({
       locator: { default: this.errorMsg },
       state: 'visible',
