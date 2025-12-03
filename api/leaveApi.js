a113c8099e3eb4e03f1fe1456aaa302ee6e48b3b
gmimran <mostafa.imran@vivasoftltd.com>
2025-12-03 16:48:14 +0600

    Revert "Merge pull request 'test/dashboard' (#25) from test/dashboard into master"
    This reverts commit 65aae294e68f7231d7e26d5cb39d984e72234383, reversing
    changes made to a2257058934652a53657985221eaf2f878831ab3.

--- api/leaveApi.js
+++
@@ -1,6 +0,0 @@
-import { getDataFromApi } from "../utils/apiUtils";
-
-export async function getCurrentLeaveBalance(employeeId) {
-    const response = await getDataFromApi(`api/v2/current-leave-statuses/${employeeId}/current-leave-balances`);
-    return response.leave_balances;
-}
