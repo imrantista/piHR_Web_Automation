ac3234803266806ea73cad58e05959071b75697d
Tanzim Emon <tanzim.emon@vivasoftltd.com>
2025-12-21 13:37:38 +0600

    Updated employee.spec.js

--- tests/functional/Employee/employee.spec.js
+++ tests/functional/Employee/employee.spec.js
@@ -1,82 +1,4 @@
 import { expect } from '@playwright/test';
-import { setViewport, Laptop, Mobile, Desktop, Tablet } from '../../../utils/viewports.js';
-import { test, allAdmin, admin ,employee, supervisor} from '../../../utils/sessionUse.js';
-import { config } from '../../../config/testConfig.js';
-
-// Create Document Request
-for (const vp of [Desktop]) {
-    test(`Employee-${vp.name} Create Document Request Application: @Self-1079`,
-    async ({ page, loginPage, useSession, documentRequest }) => {
-       
-        await setViewport(page, vp.size);
-    
-        await useSession(employee);
-      
-        await loginPage.visit(config.slug.documentRequest);
-       
-        await documentRequest.createRequestDocument()
-    });
-}
-
-// Edit Document Request
-for (const vp of [Desktop]) {
-    test(`Employee-${vp.name} Updated Document Request Application: @Self-1080`,
-    async ({ page, loginPage, useSession, documentRequest }) => {
-       
-        await setViewport(page, vp.size);
-    
-        await useSession(employee);
-      
-        await loginPage.visit(config.slug.documentRequest);
-       
-        await documentRequest.editRequestedDocument()
-    });
-}
-
-// Delete Document Request
-for (const vp of [Desktop]) {
-    test(`Employee-${vp.name} Delete Document Request Application: @Self-1081`,
-    async ({ page, loginPage, useSession, documentRequest }) => {
-       
-        await setViewport(page, vp.size);
-    
-        await useSession(employee); // 'employee'
-      
-        await loginPage.visit(config.slug.documentRequest);
-       
-        await documentRequest.deleteDocumentApplication()
-    });
-}
-
-// Approve Document Request by Admin
-for (const vp of [Desktop]) {
-    test(`Admin-${vp.name} Approve Document Request Application: @Self-1082`,
-    async ({ page, loginPage, useSession, documentRequest }) => {
-       
-        await setViewport(page, vp.size);
-        await useSession(admin);
-        await loginPage.visit(config.slug.adminDocumentRequest);
-        await documentRequest.approveDocument()
-        await useSession (employee);
-        await loginPage.visit(config.slug.documentRequest);
-        await documentRequest.employeeVerifyDocument()
-    });
-}
-
-// Reject Document Request by Admin
-for (const vp of [Desktop]) {
-    test(`Admin-${vp.name} Reject Document Request Application: @Self-1083`,
-    async ({ page, loginPage, useSession, documentRequest }) => {
-       
-        await setViewport(page, vp.size);
-        await useSession(admin);
-        await loginPage.visit(config.slug.adminDocumentRequest);
-        await documentRequest.rejectDocument()
-        await useSession (employee);
-        await loginPage.visit(config.slug.documentRequest);
-        await documentRequest.employeeVerifyRejectDocument()
-    });
-}import { expect } from '@playwright/test';
 import { setViewport, Desktop } from '../../../utils/viewports.js';
 import { test, employee, supervisor, admin } from '../../../utils/sessionUse.js';
 import { assetConfig, config } from '../../../config/testConfig.js';
@@ -190,3 +112,78 @@ for (const vp of [Desktop]) {
     );
   }
 });
+
+// Create Document Request
+for (const vp of [Desktop]) {
+    test(`Employee-${vp.name} Create Document Request Application: @Self-1079`,
+    async ({ page, loginPage, useSession, documentRequest }) => {
+       
+        await setViewport(page, vp.size);
+    
+        await useSession(employee);
+      
+        await loginPage.visit(config.slug.documentRequest);
+       
+        await documentRequest.createRequestDocument()
+    });
+}
+
+// Edit Document Request
+for (const vp of [Desktop]) {
+    test(`Employee-${vp.name} Updated Document Request Application: @Self-1080`,
+    async ({ page, loginPage, useSession, documentRequest }) => {
+       
+        await setViewport(page, vp.size);
+    
+        await useSession(employee);
+      
+        await loginPage.visit(config.slug.documentRequest);
+       
+        await documentRequest.editRequestedDocument()
+    });
+}
+
+// Delete Document Request
+for (const vp of [Desktop]) {
+    test(`Employee-${vp.name} Delete Document Request Application: @Self-1081`,
+    async ({ page, loginPage, useSession, documentRequest }) => {
+       
+        await setViewport(page, vp.size);
+    
+        await useSession(employee); // 'employee'
+      
+        await loginPage.visit(config.slug.documentRequest);
+       
+        await documentRequest.deleteDocumentApplication()
+    });
+}
+
+// Approve Document Request by Admin
+for (const vp of [Desktop]) {
+    test(`Admin-${vp.name} Approve Document Request Application: @Self-1082`,
+    async ({ page, loginPage, useSession, documentRequest }) => {
+       
+        await setViewport(page, vp.size);
+        await useSession(admin);
+        await loginPage.visit(config.slug.adminDocumentRequest);
+        await documentRequest.approveDocument()
+        await useSession (employee);
+        await loginPage.visit(config.slug.documentRequest);
+        await documentRequest.employeeVerifyDocument()
+    });
+}
+
+// Reject Document Request by Admin
+for (const vp of [Desktop]) {
+    test(`Admin-${vp.name} Reject Document Request Application: @Self-1083`,
+    async ({ page, loginPage, useSession, documentRequest }) => {
+       
+        await setViewport(page, vp.size);
+        await useSession(admin);
+        await loginPage.visit(config.slug.adminDocumentRequest);
+        await documentRequest.rejectDocument()
+        await useSession (employee);
+        await loginPage.visit(config.slug.documentRequest);
+        await documentRequest.employeeVerifyRejectDocument()
+    });
+}
