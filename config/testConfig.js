208ad5d0a16c9e7430f90d516b4f29871a6ee120
gmimran <mostafa.imran@vivasoftltd.com>
2025-12-22 15:57:23 +0600

    test

--- config/testConfig.js
+++ config/testConfig.js
@@ -1,5 +1,5 @@
-import {generateRandomId} from '../utils/generateRandomNumber.js';
 import dotenv from 'dotenv';
+import { generateRandomId } from '../utils/generateRandomNumber.js';
 dotenv.config();
 
 export const config = {
@@ -17,7 +17,7 @@ export const config = {
     "supervisorEmail": process.env.SUPERVISOR_EMAIL,
     "supervisorPassword": process.env.SUPERVISOR_PASSWORD,
     "resetPasswordEmail": 'shabitalahi123@gmail.com',
-    "newPassword":'Pihr@' + generateRandomId(100,999),
+    "newPassword": 'Pihr@' + generateRandomId(100, 999),
     "employeeAdminEmail": process.env.EMPLOYEE_ADMIN_EMAIL,
     "employeeAdminPassword": process.env.EMPLOYEE_ADMIN_PASSWORD,
     "deactivatedAdmin": process.env.DEACTIVATED_ADMIN,
@@ -43,17 +43,23 @@ export const config = {
     "advanceSalary": 'salary/advancesalary',
     "employeeasset": '/selfservice/myassignedasset',
     "assetrequisitionrequest": 'employee/assetrequisitionrequest',
+    "myTask": 'selfservice/task',
+    "task": 'employee/task',
+    "myDashboard": 'selfservice/mydashboard',
+    "documentRequest": 'selfservice/mydocumentrequest',
+    "adminDocumentRequest" : 'employee/employeedocumentrequest',
     "employeeInTimeOutTime": 'selfservice/myattendance/inouttime',
     "attendancedashboardAdmin": 'attendance/attendancedashboard',
 
+
   },
   "data": {
-    //  "emplyeeName":"Tanzim Emon",
-     "leaveType":"Annual Leave",
-     "leaveStartDate":"13-12-2025",
-     "leaveEndDate":"15-12-2025",
-     "leavePurpose":"Vacation",
-     "deleteEmployeeName":"Tanzim",
+   //  "emplyeeName": "Tanzim Emon",
+    "leaveType": "Annual Leave",
+    "leaveStartDate": "13-12-2025",
+    "leaveEndDate": "15-12-2025",
+    "leavePurpose": "Vacation",
+    "deleteEmployeeName": "Tanzim",
      "updateLeaveEndDate":"24-12-2025",
      "supEditLeaveDate":"24-12-2025",
      "adminEditLeaveDate":"24-12-2025",
@@ -61,12 +67,12 @@ export const config = {
      "newLeaveEndDate": "",
   },
   "visitApplicationData": {
-     "visitFromtDate":"19-12-2025",
-     "visitEndDate":"20-12-2025",
-     "visitPurpose":"Vacation",
-     "employeeName":"Tanzim Emon",
-     "updatedVisitPurpose":"Client Visit",
-     "updatedVisitFromDate":"18-12-2025",
+    "visitFromtDate": "19-12-2025",
+    "visitEndDate": "20-12-2025",
+    "visitPurpose": "Vacation",
+    "employeeName": "Tanzim Emon",
+    "updatedVisitPurpose": "Client Visit",
+    "updatedVisitFromDate": "18-12-2025",
   },
   "deleteApplicationData": {
     "visitReason": "Vacation"
@@ -84,7 +90,13 @@ export const config = {
     "dayCount"     : "2",
     "visitPurpose" : "d",
     "status"       : "Pending"
-  }
+  },
+   "documentRequest": {
+    "documentPurpose": "Personal",
+    "remarks": "NOC",
+    "expectedDate": "22-12-2025",
+    "updatedDocumentPurpose": "Official",
+  },
 }
 
 export const invalidCredentials = [
@@ -133,16 +145,16 @@ export const validUsers = [
   { name: 'Admin', email: config.credentials.adminEmail, password: config.credentials.adminPassword },
   { name: 'Employee', email: config.credentials.employeeEmail, password: config.credentials.employeePassword },
   { name: 'Emplyee Admin', email: config.credentials.employeeAdminEmail, password: config.credentials.employeeAdminPassword },
-  { name : 'Supervisor', email: config.credentials.supervisorEmail, password: config.credentials.supervisorPassword }
+  { name: 'Supervisor', email: config.credentials.supervisorEmail, password: config.credentials.supervisorPassword }
 ];
 
 export const branches = ['Banani', 'bKash', 'PiHR', 'Financfy', 'Kathmandu', 'Klikit', 'Mirpur DOHS', 'PiHR Support Rajshahi', 'Rajshahi'];
 
 export const invalidMobileNumbers = [
-    "017",         
-    "01712345678912414",     
-    "abc12345678",      
-    "01712@#$%1"        
+  "017",
+  "01712345678912414",
+  "abc12345678",
+  "01712@#$%1"
 ];
 //Attendance report config data
 export const reportConfig = {
@@ -152,6 +164,26 @@ export const reportConfig = {
 //Asset config data
 export const assetConfig = {
   emplyeeName: "Tanzim"
+};
+
+export const taskStatusOptions = [
+  "Pending",
+  "In Progress",
+  "Completed",
+  "Uncompleted ",
+  "Need More Time"
+];
+
+export const taskPriorityOptions = [
+  "Low",
+  "Medium",
+  "High",
+  "My Priority"
+];
+
+export const taskEmployee = {
+  code: "00000276",
+  name: "Tanzim Emon"
 };
 //Attendance config data
 export const attendanceConfig = {
